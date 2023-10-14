29adba649fb0e1d13c1c98a362e25a8ffd324f2b
verzajr <acverza.jr@gmail.com>
2023-10-14 16:46:04 -0500

    fix: locastorage clear

--- frontend/src/services/3_buttonFunctions.js
+++ frontend/src/services/3_buttonFunctions.js
@@ -1,13 +1,12 @@
 import { API_GeneralSearch, API_ArtistSearch, API_AlbumSearch, API_TrackSearch } from "./4_sourceAPI.js";
 import processGeneralSearchResults from "./5_generalSearchDataProcessing.js";
 
-export async function handleSearchClick() {
+export function handleSearchClick() {
+
 
     const searchFieldValue = document.getElementById("searchField").value; // gets value from search field
     console.log(`Handling Search for "${searchFieldValue}"...`);
-    const generalSearchResult = JSON.parse(localStorage.getItem("searchResults"));
-    if (generalSearchResult){localStorage.clear};
-    await API_GeneralSearch(searchFieldValue); // calls API function to search the API repository and gets a Json with results
+    API_GeneralSearch(searchFieldValue); // calls API function to search the API repository and gets a Json with results
     generalSearchResult = JSON.parse(localStorage.getItem("searchResults"));
     processGeneralSearchResults(searchFieldValue, generalSearchResult);// calls function to process the Json and gets arrays with info to show on the screen
     window.location.href = "#/search";
