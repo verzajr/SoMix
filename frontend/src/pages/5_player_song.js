1d3b2912b83da3f44ed96369bdcc0dae06ed6009
verzajr <acverza.jr@gmail.com>
2023-10-15 04:28:26 -0500

    fix image

--- frontend/src/pages/5_player_song.js
+++ frontend/src/pages/5_player_song.js
@@ -89,12 +89,12 @@ function createButtonsAndPlaylist(currentSong) {
     let curr_time = document.querySelector(".current-time");
     let total_duration = document.querySelector(".total-duration");
 
-    track_art.src = `url("${currentSong[0].album.cover_xl}")`;
+    track_art.setAttribute("src", `${currentSong[0].album.cover_xl}`);
     track_name.innerHTML = currentSong[0].title;
     track_artist.innerHTML=currentSong[0].artist.name;
     now_playing.innerHTML="playing 1 of 1";
-    let total_duration_min = (currentSong[0].duration)%60;
-    let total_duration_sec = Number(((currentSong[0].duration) - (currentSong[0].duration%60))*60);
+    let total_duration_min = Number(currentSong[0].duration) % 60;
+    let total_duration_sec = Number(((currentSong[0].duration) - ((currentSong[0].duration)%60))*60);
     total_duration.innerHTML= total_duration_min + ":" + total_duration_sec;
     curr_time.innerHTML=htmlAudio.currentTime;
 
@@ -102,9 +102,9 @@ function createButtonsAndPlaylist(currentSong) {
     const showRangeProgress = (rangeInput) => {
         const htmlAudio = document.getElementsByClassName('audioPlayer');
         if(rangeInput === seek_slider) {
-          htmlAudio.style.setProperty('--seek-before-width', rangeInput.value / rangeInput.max * 100 + '%');
+          htmlAudio.setAttribute('style','--seek-before-width', rangeInput.value / rangeInput.max * 100 + '%');
         } else {
-          htmlAudio.style.setProperty('--volume-before-width', rangeInput.value / rangeInput.max * 100 + '%');
+          htmlAudio.setAttribute('style', '--volume-before-width', rangeInput.value / rangeInput.max * 100 + '%');
         }
     }
     
