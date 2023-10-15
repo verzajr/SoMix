25c3ba0bd63f9c8fbd5277838bd36f7f1d1f1c48
verzajr <acverza.jr@gmail.com>
2023-10-15 01:23:24 -0500

    feat songPlayer

--- frontend/src/pages/5_player_song.js
+++ frontend/src/pages/5_player_song.js
@@ -1,9 +1,24 @@
 import { createCustomHTMLElement } from "../services/6_HTMLbuilder.js";
 
 export function buildSongPlayerPage() {
-    const sourceURL = localStorage.getItem('executionSourceURL');
-    createPlayer(sourceURL);
-    createButtonsAndPlaylist(sourceURL);
+    /* const url = localStorage.getItem('Execute_url');
+    createPlayer(url); */
+    let trackID = JSON.parse(localStorage.getItem('trackID'));
+    let trackIMAGE = JSON.parse(localStorage.getItem('trackIMAGE'));
+    let currentSong = JSON.parse(localStorage.getItem(`searchResultsFor_${trackID}`));
+
+    if (typeof (currentSong) !== Array) {
+        trackIMAGE = [trackIMAGE];
+        trackID = [trackID];
+        currentSong = [currentSong];
+    };
+
+    console.log('current song object', currentSong);
+    console.log('trackID', trackID);
+    console.log('trackIMAGE', trackIMAGE);
+
+    createButtonsAndPlaylist(currentSong, trackIMAGE);
+    createPlayer(currentSong);
 };
 
 
@@ -22,12 +37,31 @@ function callPauseButton(audio) {
     document.getElementsByClassName('playButton')[0].style.display = "inline-block";
 }
 
-
-function createPlayer(link) {
+/* function createPlayer(url) {
+  
     const songPlayer = document.getElementsByClassName('songPlayer')[0];
     const htmlAudio = songPlayer.appendChild(createCustomHTMLElement('audio', 'audioPlayer', ''));
     const source = htmlAudio.appendChild(createCustomHTMLElement('source', '', ''));
-    source.setAttribute('src', link);
+    source.setAttribute('src', url);
+    source.setAttribute('type', 'audio/mpeg');
+
+    //building control buttons
+    const audioControls = document.getElementById("audioControls");
+    const playButton = audioControls.appendChild(createCustomHTMLElement('button', 'playButton', ''));
+    playButton.addEventListener('click', () => { callPlayButton(htmlAudio) });
+    const pauseButton = audioControls.appendChild(createCustomHTMLElement('button', 'pauseButton', ''));
+    pauseButton.addEventListener('click', () => { callPauseButton(htmlAudio) });
+    document.getElementsByClassName('pauseButton')[0].style.display = "none";
+};
+ */
+
+function createPlayer(currentSong) {
+
+    const songPlayer = document.getElementsByClassName('songPlayer')[0];
+    const htmlAudio = songPlayer.appendChild(createCustomHTMLElement('audio', 'audioPlayer', ''));
+    const source = htmlAudio.appendChild(createCustomHTMLElement('source', '', ''));
+    const url = currentSong.preview;
+    source.setAttribute('src', url);
     source.setAttribute('type', 'audio/mpeg');
 
     //building control buttons
@@ -39,7 +73,7 @@ function createPlayer(link) {
     document.getElementsByClassName('pauseButton')[0].style.display = "none";
 };
 
-function createButtonsAndPlaylist(link) {
+function createButtonsAndPlaylist(currentSong, trackIMAGE) {
     let now_playing = document.querySelector(".now-playing");
     let track_art = document.querySelector(".track-art");
     let track_name = document.querySelector(".track-name");
@@ -51,123 +85,98 @@ function createButtonsAndPlaylist(link) {
     let curr_time = document.querySelector(".current-time");
     let total_duration = document.querySelector(".total-duration");
 
-    // Specify globally used values
-    let track_index = 0;
     let updateTimer;
 
-    let curr_track = link;
+    let curr_track = document.getElementsByClassName('audioPlayer');
 
     // Define the list of tracks that have to be played
-    let track_list = [];
+    let track_list = currentSong;
 
     function playTrack(audio) {
         callPlayButton(audio)
-    }
-
-    function loadTrack(track_index) {
-        // Clear the previous seek timer
-        clearInterval(updateTimer);
-        resetValues();
-
-        // Load a new track
-        curr_track.src = track_list[track_index].path;
-        curr_track.load();
-
-        // Update details of the track
-        track_art.style.backgroundImage =
-            "url(" + track_list[track_index].image + ")";
-        track_name.textContent = track_list[track_index].name;
-        track_artist.textContent = track_list[track_index].artist;
-        now_playing.textContent =
-            "PLAYING " + (track_index + 1) + " OF " + track_list.length;
-
-        // Set an interval of 1000 milliseconds
-        // for updating the seek slider
-        updateTimer = setInterval(seekUpdate, 1000);
-
-        // Move to the next track if the current finishes playing
-        // using the 'ended' event
-        curr_track.addEventListener("ended", nextTrack);
-
-    }
-
-
-    // Function to reset all values to their default
-    function resetValues() {
-        curr_time.textContent = "00:00";
-        total_duration.textContent = "00:00";
-        seek_slider.value = 0;
-    }
-
- 
-    function nextTrack() {
-        // Go back to the first track if the
-        // current one is the last in the track list
-        if (track_index < track_list.length - 1)
-            track_index += 1;
-        else track_index = 0;
-
-        // Load and play the new track
-        loadTrack(track_index);
-        playTrack();
-    }
-
-    function prevTrack() {
-        // Go back to the last track if the
-        // current one is the first in the track list
-        if (track_index > 0)
-            track_index -= 1;
-        else track_index = track_list.length - 1;
-
-        // Load and play the new track
-        loadTrack(track_index);
-        playTrack();
-    }
-    function seekTo() {
-        // Calculate the seek position by the
-        // percentage of the seek slider 
-        // and get the relative duration to the track
-        let seekto = curr_track.duration * (seek_slider.value / 100);
-
-        // Set the current track position to the calculated seek position
-        curr_track.currentTime = seekto;
-    }
-
-    function setVolume() {
-        // Set the volume according to the
-        // percentage of the volume slider set
-        curr_track.volume = volume_slider.value / 100;
-    }
-
-    function seekUpdate() {
+    } 
+    
+    function seekUpdate(curr_track) {
         let seekPosition = 0;
-
+    
         // Check if the current track duration is a legible number
         if (!isNaN(curr_track.duration)) {
             seekPosition = curr_track.currentTime * (100 / curr_track.duration);
             seek_slider.value = seekPosition;
-
+    
             // Calculate the time left and the total duration
             let currentMinutes = Math.floor(curr_track.currentTime / 60);
             let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
             let durationMinutes = Math.floor(curr_track.duration / 60);
             let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
-
+    
             // Add a zero to the single digit time values
             if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
             if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
             if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
             if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
-
+    
             // Display the updated duration
             curr_time.textContent = currentMinutes + ":" + currentSeconds;
             total_duration.textContent = durationMinutes + ":" + durationSeconds;
         }
     }
-    // Load the first track in the tracklist
-    /* loadTrack(track_index); */
 
-}
+    // Clear the previous seek timer
+    clearInterval(updateTimer);
+    resetValues();
+
+    for (let index = 0; index < track_list.length; index++) {
+        curr_track = document.getElementsByClassName('audioPlayer');
+        const song = track_list[index];
+
+        curr_track.src = song.preview;
+        curr_track.load();
+
+        track_art.style.backgroundImage = "url(" + trackIMAGE[index] + ")";
+        track_name.textContent = song.title;
+
+        now_playing.textContent = "Playing " + (track_index + 1) + " song of " + track_list.length;
+
+        updateTimer = setInterval(seekUpdate, 1000);
+
+        playTrack();
+
+       
+        const prevButton = document.getElementsByClassName('prev-track');
+        prevButton.addEventListener('click', () => {
+            if (index >= 0 && index <= track_list.length)
+                index -= 1;
+            else index = 0;
+        });
+
+        const nextButton = document.getElementsByClassName('next-track');
+        nextButton.addEventListener('click', () => {
+            if (index < track_list.length - 1)
+                index += 1;
+            else index = 0;
+        });
+
+        seek_slider.addEventListener('click', () => {  
+        const song_seekto = curr_track.duration * (seek_slider.value / 100);
+        curr_track.currentTime = song_seekto});
+
+        volume_slider.addEventListener('click', () => { curr_track.volume = volume_slider.value / 100 });
+
+        curr_time.addEventListener('click', seekUpdate(curr_track));
+        total_duration.addEventListener('click', seekUpdate(curr_track));
+
+
+        curr_track.addEventListener("ended", () => {
+            curr_time.textContent = "00:00";
+            total_duration.textContent = "00:00";
+            seek_slider.value = 0;
+            if (index < track_list.length - 1)
+                index += 1;
+            else index = 0;
+        });
+    };
+};
 
 
 export const executeSongComponent = {
--- frontend/src/services/4_sourceAPI.js
+++ frontend/src/services/4_sourceAPI.js
@@ -13,25 +13,25 @@ export async function API_GeneralSearch(searchValue, callback) {
 };
 
 
-export function API_ArtistSearch(artistID) {
+export async function API_ArtistSearch(artistID) {
 
-    DZ.api(`/artist/${artistID}`, function (response) {
+    await DZ.api(`/artist/${artistID}`, function (response) {
         localStorage.setItem(`searchResultsFor_${artistID}`, JSON.stringify(response));
     });
 
 };
 
-export function API_AlbumSearch(albumID) {
+export async function API_AlbumSearch(albumID) {
 
-    DZ.api(`/album/${albumID}`, function (response) {
+    await DZ.api(`/album/${albumID}`, function (response) {
         localStorage.setItem(`searchResultsFor_${albumID}`, JSON.stringify(response));
     });
 
 };
 
-export function API_TrackSearch(trackID) {
+export async function API_TrackSearch(trackID) {
     
-    DZ.api(`/track/${trackID}`, function (response) {
+    await DZ.api(`/track/${trackID}`, function (response) {
         localStorage.setItem(`searchResultsFor_${trackID}`, JSON.stringify(response));
     });
 
